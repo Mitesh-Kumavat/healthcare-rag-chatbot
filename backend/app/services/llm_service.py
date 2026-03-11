13f476c73f7e4ab7054269b04187b88d8a45da39
mitesh-kumavat-upforce <mitesh.kumavat@upforce.tech>
2026-03-11 12:54:31 +0530

    fix: change model HF

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -45,7 +45,7 @@ def get_gemini_model() -> ChatGoogleGenerativeAI:
 @lru_cache()
 def get_huggingface_model() -> ChatHuggingFace:
     llm = HuggingFaceEndpoint(
-        repo_id="perplexity-ai/r1-1776",
+        repo_id="meta-llama/Llama-3.1-8B-Instruct",
         task="text-generation",
         max_new_tokens=512,
     )
